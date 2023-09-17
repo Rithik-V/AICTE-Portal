1cd93f6af78509fa52b6c86606959e2d51eaf98a
Rithik V <rithikvijaykumar@gmail.com>
2023-09-17 14:24:27 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Logincard from './components/Logincard';
+import Frontpage from './components/frontpage';
+
+function App() {
+  return (
+    <Frontpage></Frontpage>
+  );
+}
+
+export default App;
