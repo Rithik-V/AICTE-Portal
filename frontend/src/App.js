ee7688303265177eccb81ff39fe36eea8e7fccc3
Rithik V <rithikvijaykumar@gmail.com>
2023-09-17 23:15:39 +0530

    Average for each k level calculated and console logged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Logincard from './components/Logincard';
 import Frontpage from './components/frontpage';
-
+import Kavg from './components/kavg';
 function App() {
   return (
     <div id="landing">
       <Frontpage></Frontpage>
       <Logincard></Logincard>
+      <Kavg></Kavg>
     </div>
     
   );
