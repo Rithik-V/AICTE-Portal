f45f882522b65371545d14a9c109549c91c2514b
Rithik V <rithikvijaykumar@gmail.com>
2023-09-20 19:34:22 +0530

    Added Basic design for course page

---
+++ frontend/src/screens/Coursepage.js
@@ -0,0 +1,25 @@
+import "./Coursepage.css"
+function Coursepage()
+{
+    return (
+        <>
+        <div className="coursepage">
+            <div className="course">
+                <div className="coursetit">
+                    <p className="coursename">Electronics and communication</p>
+                </div>
+            </div>
+            
+        </div>
+        <div className="coursepage">
+        <div className="course">
+            <div className="coursetit">
+                <p className="coursename">Computer Science and Engineering</p>
+            </div>
+        </div>
+        
+    </div>
+    </>
+    )
+}
+export default Coursepage;
