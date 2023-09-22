334207f96baac00bc2ce5b56d53ca6eecfb05469
Rithik V <rithikvijaykumar@gmail.com>
2023-09-23 00:00:11 +0530

    created version control for course curriculum

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import HomePage from './screens/HomePage';
 import Coursepage from "./screens/Coursepage"
 import Kavg from "./components/kavg"
+import Cirriculum from './screens/curriculum';
 const App = () => {
   return (
-    <Coursepage/>
+    <Cirriculum/>
   );
 }
 
