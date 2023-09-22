a31d4bf913436eab374c4e37e5ccd92e56741d51
Rithik V <rithikvijaykumar@gmail.com>
2023-09-22 23:03:32 +0530

    Created hardcoded full course page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,10 @@
-/*import './App.css';
-import Logincard from './components/Logincard';
-import Frontpage from './components/frontpage';
-import Kavg from './components/kavg';
-function App() {
-  return (
-    <div id="landing">
-      <Frontpage></Frontpage>
-      <Logincard></Logincard>
-      <Kavg></Kavg>
-    </div>
-    
-  );
-}
-
-export default App;
-*/
 import './App.css';
-import {Container} from "react-bootstrap";
 import HomePage from './screens/HomePage';
-
+import Coursepage from "./screens/Coursepage"
+import Kavg from "./components/kavg"
 const App = () => {
   return (
-    <>
-      <HomePage/>
-      <main className='py-3'>
-        <Container>
-        <h1>Welcome</h1>
-        </Container>
-      </main>
-    </>
+    <Coursepage/>
   );
 }
 
--- frontend/src/components/kavg.js
+++ frontend/src/components/kavg.js
@@ -3,9 +3,7 @@ import * as XLSX from "xlsx";
 
 
 function Kavg() {
-
   const [data, setData] = useState([]);
-
   const handleFileUpload = (e) => {
     const reader = new FileReader();
     reader.readAsBinaryString(e.target.files[0]);
